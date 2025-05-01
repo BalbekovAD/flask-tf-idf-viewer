3953c610dd7de28e2fc39213c3f28c1433ea3eac
Balbekov A. D. <72185334+BalbekovAD@users.noreply.github.com>
2025-05-01 20:13:16 +0300

    Implement viewer
    Write small server that hosts a site that allows to calculate tfidf for multiple files.

---
+++ app.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from typing import Iterable
+from xml.dom.expatbuilder import DOCUMENT_NODE
+
+from flask import Flask, render_template, request, redirect, flash
+from numpy.random.mtrand import Sequence
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+from pandas import DataFrame
+from numpy.typing import NDArray
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@dataclass
+class TFIDFEntry:
+    word: str
+    tf: int
+    idf: float
+
+
+@dataclass
+class DocumentData:
+    filename: str
+    tfidf_data: Sequence[TFIDFEntry]
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    files = request.files.getlist('file')
+    return render_template(
+        'file_page.html',
+        files_analysis=list(analyze_files(files))
+    )
+
+
+def analyze_files(files: Sequence[FileStorage]) -> Iterable[DocumentData]:
+    vectorizer: TfidfVectorizer = TfidfVectorizer(input='file', norm=None)
+
+    tfidf_matrix = vectorizer.fit_transform(files)
+    words = vectorizer.get_feature_names_out()
+    idf_values = vectorizer.idf_
+    tf_matrix = tfidf_matrix.toarray() / idf_values
+
+    for i, file in enumerate(files):
+        filename = secure_filename(file.filename)
+
+        yield DocumentData(
+            filename,
+            sorted(
+                (TFIDFEntry(word, int(tf), round(idf, 3)) for word, tf, idf in zip(words, tf_matrix[i], idf_values) if tf != 0),
+                key=lambda x: x.idf,
+                reverse=True
+            )[:50]
+        )
+
+
+if __name__ == '__main__':
+    app.run()
